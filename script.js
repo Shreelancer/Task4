97b64f3407c1e9938dbf5eb21381bf7938ca2cf9
Shreyash Pani <68473243+Shreelancer@users.noreply.github.com>
2021-12-26 17:28:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function ValidateUser(inputText,password)
+{       
+        var valid = 0;
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        
+        if(inputText.value === "" || password.value == ""){
+            alert("Please Enter username and password");
+        }
+        if(inputText.value.match(mailformat))
+        {
+            valid = 1;
+        }
+        if(valid == 1){
+            if (password.value.search(/[A-Z]/i) < 0) {
+                valid = 0;
+                alert("Your password must contain at least one upper case letter.");
+            }
+            else if (password.value.search(/[a-z]/i) < 0) {
+                valid = 0;
+                alert("Your password must contain at least one letter.");
+            }
+            else if (password.value.search(/[0-9]/) < 0) {
+                valid = 0;
+                alert("Your password must contain at least one digit."); 
+            }
+            else if (password.value.search(/[!#$%^&*]/) > 0) {
+                valid = 0;
+                alert("Your password must not contain special character other than @"); 
+            }
+            else{
+               if(password.value == "SmartServTest@123"){
+                    window.location.assign("");
+               }
+               else{
+                   alert("Wrong Password please Check!");
+               }
+            }
+        }
+        else{
+            alert("Your Email is not Valid please Check!")
+        }     
+}
